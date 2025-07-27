4a103013b64cada4afd0d2fc7d77f5afe18ec8e8
ByteAtATime <byteatatime@proton.me>
2025-07-27 15:24:53 -0700

    feat: basic phrase tokenization

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -1,3 +1,5 @@
+import dictionary from '$lib/compounds.txt?raw';
+
 export type Translation = { toki: string; literal: string; original: string };
 
 export const matchLines = (translation: Translation): Translation[] => {
@@ -14,4 +16,58 @@ export const matchLines = (translation: Translation): Translation[] => {
 		literal: literalLines[i] ?? '',
 		original: originalLines[i] ?? ''
 	}));
+};
+
+const getDictionaryDefinitions = (dictionary: string) =>
+	new Map(
+		dictionary
+			.matchAll(/([\w ]+):\s*\[(.+)\]/g)
+			.map((match) => [match[1], match[2]])
+			.map(([term, rawTranslations]) => [
+				term,
+				rawTranslations
+					.split(',')
+					.map((translation) => translation.trim().match(/(.+)\s+\d+/)?.[1])
+					.filter((x) => x) as string[]
+			])
+	);
+
+type Token = {
+	phrase: string;
+	definition: string[] | undefined;
+};
+
+export const tokenizeTokiPona = (sentence: string): Token[] => {
+	const validChars = new Set('aeijklmnopstuw '); // space is intentional
+	const cleanedString = sentence
+		.split('')
+		.map((c) => c.toLowerCase())
+		.filter((c) => validChars.has(c))
+		.join('')
+		.trim();
+
+	const words = cleanedString.split(' ');
+
+	const definitions = getDictionaryDefinitions(dictionary);
+	const maxSubsetLength = Math.max(...definitions.keys().map((key) => key.split(' ').length));
+
+	const tokens: Token[] = [];
+
+	for (let i = 0; i < words.length; ) {
+		for (
+			let wordsToConsume = Math.min(maxSubsetLength, words.length - i);
+			wordsToConsume >= 1;
+			wordsToConsume--
+		) {
+			const phrase = words.slice(i, i + wordsToConsume).join(' ');
+			if (definitions.has(phrase)) {
+				tokens.push({ phrase, definition: definitions.get(phrase) });
+				i += wordsToConsume;
+				break;
+			}
+		}
+	}
+
+	console.log(tokens);
+	return tokens;
 };
